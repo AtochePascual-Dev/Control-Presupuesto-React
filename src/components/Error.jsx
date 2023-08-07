b86c013b1a577a48f44a2cb3bf4ba2201b6a278f
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-06 17:25:14 -1000

    [FEATURE] error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+
+
+  return (
+    <div className="my-5 p-3 font-bold uppercase text-center rounded-md text-white bg-red-700">
+      <p>Presupuesto no valido</p>
+    </div>
+  )
+}
+
+export default Error
