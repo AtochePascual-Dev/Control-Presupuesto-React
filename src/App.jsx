b52fe55495d333a0f10f9c700b6ee0dfb18cd36b
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-06 15:42:31 -1000

    [FEATURE] header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="w-11/12 mx-auto mt-10 py-5 rounded-md bg-white">
+      <Header />
+
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="">
+      <h1 className="text-4xl text-center font-extrabold">Controla tu <span className="text-indigo-600"> Presupuesto</span></h1>
+    </header>
+  )
+}
+
+export default Header
